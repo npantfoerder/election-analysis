9285ec150bf488e869115536a4f2d2203363c153
Neele Pantfoerder <neele@NeelePardersMBP.lan>
2020-07-27 22:29:55 -0700

    Added analysis folder

--- PyPoll.py
+++ PyPoll.py
@@ -4,3 +4,24 @@
 # 3) The percentage of voes each candidate won
 # 4) The total number of votes each candidate won
 # 5) The winner of the election based on popular vote
+
+import csv
+import os
+
+# Assign a variable for the file to laod and the path
+file_to_load = 'Resources/election_results.csv'
+#file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Open the election results and read the file
+with open(file_to_load, 'r') as election_data:
+    # To do: perform analytics
+    print(election_data)
+
+# Create a filename variable to a direct path to the file 
+file_to_save = 'analysis/election_analysis.txt'
+
+# Write data to the file
+with open(file_to_save, 'w') as out_file:
+    # Write data to the file
+    out_file.write("Hello World")
+    
--- Python_practice.py
+++ Python_practice.py
@@ -1,5 +1,7 @@
 counties_dict = {"Arapahoe":422829, "Denver":463353, "Jefferson":432438}
 voting_data = [{"county":"Arapahoe", "registered_voters":422829}, {"county":"Denver", "registered_voters":463353}, {"county":"Jefferson", "registered_voters":432438}]
 
-for dict in voting_data:
-    print(f"{dict['county']} county has {dict['registered_voters']:,} registered voters.")
+import datetime as dt
+now = dt.datetime.now()
+# print("The time right now is ", now)
+
