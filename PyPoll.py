886db7463675dfcb4b59a5ced40341d1b806d0c9
Neele Pantfoerder <neele@NeelePardersMBP.lan>
2020-07-27 23:19:01 -0700

    Updated PyPoll.py file

--- PyPoll.py
+++ PyPoll.py
@@ -5,20 +5,23 @@
 # 4) The total number of votes each candidate won
 # 5) The winner of the election based on popular vote
 
+# Import dependencies
 import csv
 import os
 
-# Assign a variable for the file to laod and the path
+# Create a variable for a path to load the file
 file_to_load = 'Resources/election_results.csv'
-#file_to_load = os.path.join("Resources", "election_results.csv")
+# Create a variable for a path to save the file 
+file_to_save = 'analysis/election_analysis.txt'
 
 # Open the election results and read the file
 with open(file_to_load, 'r') as election_data:
     # To do: perform analytics
-    print(election_data)
+    file_reader = csv.reader(election_data)
 
-# Create a filename variable to a direct path to the file 
-file_to_save = 'analysis/election_analysis.txt'
+    # Print the header row
+    headers = next(file_reader)
+    print(headers)
 
 # Open the file as a text file
 with open(file_to_save, 'w') as txt_file:
