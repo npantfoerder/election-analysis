f616f1cfd955ca32861c213fe4ebdd8f850b4edf
Neele Pantfoerder <neele@NeelePardersMBP.lan>
2020-07-28 20:35:31 -0700

    Removed Python_practice.py

--- Python_practice.py
+++
@@ -1,7 +0,0 @@
-counties_dict = {"Arapahoe":422829, "Denver":463353, "Jefferson":432438}
-voting_data = [{"county":"Arapahoe", "registered_voters":422829}, {"county":"Denver", "registered_voters":463353}, {"county":"Jefferson", "registered_voters":432438}]
-
-import datetime as dt
-now = dt.datetime.now()
-# print("The time right now is ", now)
-
